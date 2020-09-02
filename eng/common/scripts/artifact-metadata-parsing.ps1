fef0f0d141af5ab5ce8651b4a40f4a63039e5766
Sima Zhu <sizhu@microsoft.com>
2020-09-01 17:19:38 -0700

    fixed wrong function call

--- eng/common/scripts/artifact-metadata-parsing.ps1
+++ eng/common/scripts/artifact-metadata-parsing.ps1
@@ -390,7 +390,7 @@ function GetExistingTags($apiUrl) {
 # Retrieve release tag for artiface package. If multiple packages, then output the first one.
 function RetrieveReleaseTag($pkgRepository, $artifactLocation, $continueOnError = $true) {
   try {
-    $pkgsInfo = RetrievePackages $pkgRepository, $artifactLocation
+    $pkgsInfo, $ParsePkgInfoFn = RetrievePackages $pkgRepository, $artifactLocation
     $pkgs = $pkgsInfo[0]
     if (!pkgs -or !$pkgs[0]) {
       return ""
